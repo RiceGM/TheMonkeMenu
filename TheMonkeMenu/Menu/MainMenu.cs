a564589c03fd6c90db462b4de069f52adc692809
RiceGM <off.ricegm@gmail.com>
2023-06-10 12:53:25 +0300

    Add project files.

---
+++ TheMonkeMenu/Menu/MainMenu.cs
@@ -0,0 +1,62 @@
+﻿using Photon.Pun;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+using UnityEngine.XR;
+
+namespace TheMonkeMenu.Menu
+{
+    public class MainMenu : MonoBehaviourPunCallbacks
+    {
+        public MonkePatcher monkePatcher;
+
+        public bool[] modsEnabled;
+        public string[] monkeMods = new string[]
+        {
+            "Fly",
+            "NoClip",
+            "Platforms"
+        };
+
+        bool aBtn, bBtn, xBtn, yBtn;
+        bool leftTrigger, rightTrigger, leftGrip, rightGrip;
+
+        public void InitializeMenu()
+        {
+            Debug.Log("Initializing Menu...");
+        }
+
+        public void Update()
+        {
+            GetMenuInputs();
+
+            if(rightGrip && CanGrabMenu())
+            {
+
+            }
+        }
+
+        void GetMenuInputs()
+        {
+            List<InputDevice> leftList = new List<InputDevice>();
+            List<InputDevice> rightList = new List<InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller, leftList);
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller, rightList);
+            leftList[0].TryGetFeatureValue(CommonUsages.gripButton, out leftGrip);
+            leftList[0].TryGetFeatureValue(CommonUsages.triggerButton, out leftTrigger);
+            leftList[0].TryGetFeatureValue(CommonUsages.primaryButton, out yBtn);
+            leftList[0].TryGetFeatureValue(CommonUsages.secondaryButton, out xBtn);
+
+            rightList[0].TryGetFeatureValue(CommonUsages.triggerButton, out rightTrigger);
+            rightList[0].TryGetFeatureValue(CommonUsages.gripButton, out rightGrip);
+            rightList[0].TryGetFeatureValue(CommonUsages.primaryButton, out bBtn);
+            rightList[0].TryGetFeatureValue(CommonUsages.secondaryButton, out aBtn);
+        }
+
+        bool CanGrabMenu()
+        {
+            float dot = Vector3.Dot(GorillaLocomotion.Player.Instance.rightHandTransform.position - GorillaLocomotion.Player.Instance.transform.position, GorillaLocomotion.Player.Instance.transform.forward);
+            return dot < -0.1f;
+        }
+    }
+}
---
+++ TheMonkeMenu/MonkePatcher.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TheMonkeMenu.Menu;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheMonkeMenu
+{
+    [BepInPlugin("org.ricegm.thatmonkemenu", "ThatMonkeMenu", "1.0.0")]
+    public class MonkePatcher : BaseUnityPlugin
+    {
+        GameObject mainMenu;
+        MainMenu mainMenuLocal;
+        Harmony harmony;
+
+        void OnEnable()
+        {
+            if (harmony == null) harmony = new Harmony("org.ricegm.thatmonkemenu");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            SceneManager.sceneLoaded += InitializeMonkeMenu;
+        }
+
+        void InitializeMonkeMenu(Scene arg0, LoadSceneMode arg1)
+        {
+            if (!mainMenu)
+            {
+                mainMenu = new GameObject("MonkeMenu");
+                GameObject.DontDestroyOnLoad(mainMenu);
+                mainMenuLocal = mainMenu.AddComponent<MainMenu>();
+                mainMenuLocal.monkePatcher = this;
+
+                mainMenuLocal.modsEnabled[0] = Config.Bind("EnabledMods", "Fly", false, "Is the Fly mod enabled?").Value;
+                mainMenuLocal.modsEnabled[1] = Config.Bind("EnabledMods", "NoClip", false, "Is the NoClip mod enabled?").Value;
+                mainMenuLocal.modsEnabled[2] = Config.Bind("EnabledMods", "Platforms", false, "Is the Platforms mod enabled?").Value;
+
+                mainMenuLocal.InitializeMenu();
+            }
+        }
+
+        void OnDisable()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        public void ChangeConfig(int modID, string modName, string modSection, bool enabled)
+        {
+            Config.TryGetEntry(modSection, modName, out ConfigEntry<bool> entry);
+            entry.Value = enabled;
+            Config.Save();
+            mainMenuLocal.modsEnabled[modID] = enabled;
+        }
+    }
+}
